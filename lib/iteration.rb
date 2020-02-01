bdf6a84d2a88283cb5efbffda089484bf9d7c724
bryan lesht <bryan.lesht@colorado.edu>
2020-02-01 05:01:34 +0000

    Automatically backed up by Learn

--- lib/iteration.rb
+++ lib/iteration.rb
@@ -7,16 +7,7 @@ def find_greater_pair(src)
 end 
 
 def total_even_pairs(src)
-  total = 0 
-  row_index = 0 
-  while row_index < src.count do 
-    if src[row_index][0] % 2 == 0 and src[row_index][1] % 2 == 0 
-      addin = src[row_index][0] + src[row_index][1]
-      total += addin 
-    end
-    row_index +=1 
-  end
-  total 
+
 end
 
 
